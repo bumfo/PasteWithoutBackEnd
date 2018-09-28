b059b6ea369c7ba2bc636cdb42d26953fc1e5ef5
bumfo <bumfo@users.noreply.github.com>
2018-09-28 22:35:53 +0800

    two-way binding

--- decode.js
+++ decode.js
@@ -3,6 +3,20 @@ function basename(str) {
   return base;
 }
 
+function Uint8ToBase64(u8Arr) {
+  var CHUNK_SIZE = 0x8000;
+  var index = 0;
+  var length = u8Arr.length;
+  var result = '';
+  var slice;
+  while (index < length) {
+    slice = u8Arr.subarray(index, Math.min(index + CHUNK_SIZE, length)); 
+    result += String.fromCharCode.apply(null, slice);
+    index += CHUNK_SIZE;
+  }
+  return btoa(result);
+}
+
 function base64ToUint8(base64) {
   var raw = window.atob(base64);
   var rawLength = raw.length;
@@ -24,42 +38,125 @@ function invalid() {
 var B = document.querySelector('#b');
 var C = document.querySelector('#c');
 
-var decode = async () => {
-  var b = B.value;
+var decode = async (e, callee) => {
+  if (callee === decode) {
+    return;
+  }
 
-  var c = b
-    .replace(/-/g, '+')
-    .replace(/_/g, '/');
+  var z = B.value;
 
-  var d = base64ToUint8(c);
+  var b = basename(z);
 
-  var e = await lzma_decompress(d);
-  C.value = e;
+  var j = b.indexOf('#');
+  if (j !== -1) {
+    b = b.substr(j + 1);
 
-  try {
-    if (C.onchange) C.onchange();
-  } catch (e) {
-    console.log(e);
+    if (b.length > 0) {
+      var c = b
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+      var d = base64ToUint8(c);
+
+      var e = await lzma_decompress(d);
+      C.value = e;
+
+      try {
+        var event = new Event('change');
+        event.callee = callee || decode;
+
+        C.dispatchEvent(event);
+      } catch (ex) {
+        console.log(ex);
+      } 
+    }
   }
 };
 
-B.oninput = decode;
+function updateB(b, e, callee) {
+  B.value = location.href.substring(0, location.href.lastIndexOf('/') + 1) + b;
 
-var a = basename(location.href);
+  // var j = b.indexOf('#');
+  // if (j !== -1) {
+  //   b = b.substr(j + 1);
+  // }
 
-var j = a.indexOf('#');
-if (j !== -1) {
-  a = a.substr(j + 1);
-} else {
-  invalid();
+  try {
+    if (B.onchange) {
+      B.onchange(e, callee || reEncode);
+    }
+  } catch (ex) {
+    console.log(ex);
+  }
 }
 
-B.value = a;
+var reEncode = async (e, callee) => {
+  if (callee === reEncode) {
+    return;
+  }
+
+  if (C.value === '' || c.value === '\n') {
+    updateB('plain', e, callee);
+
+    return;
+  }
+
+  var a = await lzma_compress(C.value);
+
+  var b = Uint8ToBase64(a)
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+
+  // console.log(b.length)
+
+  // if (b.length > 1024) {
+    b = 'plain#' + b;
+  // }
+
+  updateB(b, e, callee);
+};
+
+B.oninput = decode;
+B.onchange = function f(e) {
+  var b = B.value;
+
+  var j = b.indexOf('#');
+  if (j !== -1) {
+    b = b.substr(j + 1);
+
+    location.hash = b;
+  } else {
+    location.hash = '';
+  }
+};
+
+C.addEventListener('input', function f({callee}) {
+  if (callee === f) {
+    console.log('break input');
+    return;
+  }
+
+  reEncode(null, callee || f);
+});
+
+C.addEventListener('change', function f({callee}) {
+  if (callee === f) {
+    console.log('break decode');
+    return;
+  }
+
+  reEncode(null, callee || f);
+});
+
+var z = location.href;
+
+B.value = z;
 
 (async function() {
   try {
     await decode();
-  } catch (e) {
+  } catch (ex) {
     invalid();
   }
 }());
